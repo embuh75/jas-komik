81efe613917db0d9bda803fc3221b8cdc87b7264
embuh75 <coolbanditz75@gmail.com>
2026-08-05 12:55:28 +0700

    feat: implement routing with react-router-dom and restructure main app component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { queryClient, persister } from "./lib/queryClient.js";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister: persister }}>
-      <App />
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: persister }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </PersistQueryClientProvider>
   </StrictMode>,
 );
